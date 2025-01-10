511c565edd6bffdbaa77f7ccfe9053ac088fd49c
이상현 <firstquarterlee@gmail.com>
2025-01-11 04:13:59 +0900

    유저 인증까지 firebase로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,51 @@
 <script>
-  import Main from "./pages/Main.svelte"
-  import Login from "./pages/Login.svelte"
+  import Main from "./pages/Main.svelte";
+  import Login from "./pages/Login.svelte";
   import Signup from "./pages/Signup.svelte";
   import Write from "./pages/Write.svelte";
-  import Router from 'svelte-spa-router'
+  import Router from "svelte-spa-router";
   import NotFound from "./pages/NotFound.svelte";
-  import "./css/style.css"
+  import "./css/style.css";
+  import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { user$ } from "./store";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+  import Mypage from "./pages/Mypage.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
-    '/':Main,
-    '/login':Login,
-    '/signup':Signup,
-    '/write':Write,
-    '*':NotFound,
-  }
+    "/": Main,
+    "/signup": Signup,
+    "/write": Write,
+    "/my": Mypage,
+    "*": NotFound,
+  };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router routes={routes}/>
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
